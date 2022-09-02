e56790ef1b06346936bd719393ffc6ad043260c4
Maksym <maksymRozhko7@gmail.com>
2022-09-02 16:26:40 +0300

    feat: add preloader

--- src/js/components/preloader.js
+++ src/js/components/preloader.js
@@ -1,19 +1,13 @@
 function preloader(preloader) {
     const loader = document.querySelector(preloader);
-    const video = document.querySelector('.main-screen__video');
 
     if (loader) {
         setTimeout(() => {
-            loader.children[0].classList.add('active');
+            loader.classList.add('remove');
         }, 1500);
         setTimeout(() => {
-            loader.classList.add('remove');
-        }, 3500);
-        setTimeout(() => {
             loader.remove();
-            video.setAttribute('autoplay', 'autoplay');
-            video.play();
-        }, 5500);
+        }, 3500);
     }
 };
 
